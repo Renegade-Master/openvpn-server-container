86de62e013f87695805835e1732b8dd5054a8b1f
Renegade-Master <renegade@renegade-master.com>
2023-04-07 19:16:35 +0100

    Create generate_ca.sh

---
+++ generate_ca.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+set -x -eu -o pipefail
+
+openvpn_image="docker.io/kylemanna/openvpn"
+openvpn_version="2.4"
+image="${openvpn_image}:${openvpn_version}"
+
+data_dir="$(pwd)/data/"
+name="openvpn-ca"
+
+common_name="test.renegade-master.com"
+
+# Initialize the $OVPN_DATA container that will hold the configuration
+# files and certificates. The container will prompt for a passphrase to
+# protect the private key used by the newly generated certificate authority.
+podman --storage-opt ignore_chown_errors=true run --rm \
+    --name "${name}" \
+    --env DEBUG=1 \
+    --volume "${data_dir}":"/etc/openvpn":z \
+    "${image}" \
+        ovpn_genconfig -u "udp://${common_name}"
+
+podman run --rm \
+    --name "${name}" \
+    --env DEBUG=1 \
+    --volume "${data_dir}":"/etc/openvpn":z \
+    "${image}" \
+        ovpn_initpki
